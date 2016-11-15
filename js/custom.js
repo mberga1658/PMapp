16df283389bf834788053490c4b80421fc229c63
Tom Hall <thall1961@gmail.com>
2016-11-14 20:04:44 -0700

    Added ajax call to JSON file

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 document.getElementById("update").addEventListener("click",funtion(){
 	alert("hi");
 });
+
+function loadData(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(this.readyState = 4 && this.status = 200){
+			var dataObj = JSON.parse(this.responseText);
+			document.getElementById("test").innerHTML = dataObj;
+		}
+
+		xhttp.open("GET", "Task.JSON", true);
+		xhttp.send();
+	}
+}
