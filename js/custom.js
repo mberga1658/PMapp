c0619cdf4b2226f802b298e28795325b71af43c2
Tom Hall <thall1961@gmail.com>
2016-11-14 20:17:31 -0700

    fixed if condition

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 function loadData(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if(this.readyState = 4 && this.status = 200){
+		if(this.readyState = (4 && this.status = 200){
 			var dataObj = JSON.parse(this.responseText);
 			document.getElementById("test").innerHTML = dataObj;
 		}
